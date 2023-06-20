f2d277b02ae0e1c85961526bad44867b1afeec60
n4j1Br4ch1D <kawner93@gmail.com>
2023-06-20 14:02:43 +0100

    change cmnd name

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ with open("README.md", "r") as fh:
 setuptools.setup(
      name='shift-ip',  
      version='1.0.0',
-     scripts=['shift'] ,
+     scripts=['shift-ip'] ,
      author="Najib Rachid",
      description="SHIFT is a penetration testing tool designed to help you identify the IP addresses of shared hosting associated with a specific domain. It checks each IP address to determine if the domain is hosted on that IP.",
      long_description=long_description,
-   long_description_content_type="text/markdown",
+     long_description_content_type="text/markdown",
      url="https://github.com/n4j1Br4ch1D/SHIFT",
      packages=setuptools.find_packages(),
      classifiers=[
